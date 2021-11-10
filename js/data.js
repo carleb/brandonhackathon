ac5b7c5553132fbfdd6a150723450a6868a89c67
Caleb Cheong <calebcheongdnt@gmail.com>
2021-11-11 00:28:09 +0800

    push

---
+++ js/data.js
@@ -0,0 +1,26 @@
+formData = `[["formID#002","Pre-Departure Checklist","111","Ship Master","Brandon Chris","123","10/11/2021","Brandon Chris","Good","1.75","10","11/10/2021 11:54 PM"],
+["formID#002","Pre-Departure Checklist","112","Ship CrewMember","Caleb Chris","123","10/11/2021","Caleb Chris","Good","1.75","10","11/10/2021 11:55 PM"],
+["formID#002","Pre-Departure Checklist","113","Ship CrewMember","Rou Chris","123","10/11/2021","Rou Chris","Good","1.75","10","11/10/2021 11:56 PM"],
+["formID#002","Pre-Departure Checklist","114","Ship CrewMember","Christer Chris","123","10/11/2021","Christer Chris","Good","1.75","10","11/10/2021 11:57 PM"],
+["formID#002","Pre-Departure Checklist","115","Ship CrewMember","Ger Chris","123","10/11/2021","Ger Chris","Good","1.75","10","11/10/2021 11:58 PM"],
+["formID#002","Pre-Departure Checklist","116","Ship CrewMember","Jiazhe Chris","123","10/11/2021","Jiazhe Chris","Good","1.75","10","11/10/2021 11:59 PM"],
+["formID#002","Pre-Departure Checklist","116","Ship CrewMember","Tom Chris","123","10/11/2021","Tom Chris","Good","1.75","10","11/10/2021 11:54 PM"],
+["formID#001","Daily Ship Attendance","111","Ship Master","Brandon Chris","123","10/11/2021","Brandon Chris","Good","1.75","10","11/10/2021 11:54 PM"],
+["formID#001","Daily Ship Attendance","112","Ship CrewMember","Caleb Chris","123","10/11/2021","Caleb Chris","Good","1.75","10","11/10/2021 11:55 PM"],
+["formID#001","Daily Ship Attendance","113","Ship CrewMember","Rou Chris","123","10/11/2021","Rou Chris","Good","1.75","10","11/10/2021 11:56 PM"],
+["formID#001","Daily Ship Attendance","114","Ship CrewMember","Christer Chris","123","10/11/2021","Christer Chris","Good","1.75","10","11/10/2021 11:57 PM"],
+["formID#001","Daily Ship Attendance","115","Ship CrewMember","Ger Chris","123","10/11/2021","Ger Chris","Good","1.75","10","11/10/2021 11:58 PM"],
+["formID#001","Daily Ship Attendance","116","Ship CrewMember","Jiazhe Chris","123","10/11/2021","Jiazhe Chris","Good","1.75","10","11/10/2021 11:59 PM"],
+["formID#003","Another Form","114","Ship CrewMember","Christer Chris","123","10/11/2021","Christer Chris","Good","1.75","10","11/10/2021 11:57 PM"],
+["formID#003","Another Form","115","Ship CrewMember","Ger Chris","123","10/11/2021","Ger Chris","Good","1.75","10","11/10/2021 11:58 PM"],
+["formID#003","Another Form","116","Ship CrewMember","Jiazhe Chris","123","10/11/2021","Jiazhe Chris","Good","1.75","10","11/10/2021 11:59 PM"],
+["formID#001","Daily Ship Attendance","116","Ship CrewMember","Tom Chris","123","10/11/2021","Tom Chris","Good","1.75","10","11/10/2021 11:54 PM"],
+["formID#001","Daily Ship Attendance","116","Ship CrewMember","Tom Chris","123","10/11/2021","Tom Chris","Good","1.75","10","11/10/2021 11:54 PM"],
+["formID#001","Daily Ship Attendance","116","Ship CrewMember","Tom Chris","123","10/11/2021","Tom Chris","Good","1.75","10","11/10/2021 11:54 PM"],
+["formID#001","Daily Ship Attendance","116","Ship CrewMember","Tom Chris","123","10/11/2021","Tom Chris","Good","1.75","10","11/10/2021 11:54 PM"],
+["formID#001","Daily Ship Attendance","116","Ship CrewMember","Tom Chris","123","10/11/2021","Tom Chris","Good","1.75","10","11/10/2021 11:54 PM"],
+["formID#001","Daily Ship Attendance","116","Ship CrewMember","Tom Chris","123","10/11/2021","Tom Chris","Good","1.75","10","11/10/2021 11:51 PM"]]
+`
+
+myStorage = window.sessionStorage;
+myStorage.form1Data=formData;
